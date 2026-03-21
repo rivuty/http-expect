fc24e7f408f5ff5d65fea497d6fe9d4588a2f88d
Oleg Michailov <source.code@michailov.lt>
2026-03-21 11:33:48 +0200

    Add isOk status matcher

---
+++ src/status/status.ts
@@ -0,0 +1,16 @@
+import { HttpStatusCode } from "@rivuty/http-status";
+
+const expectCode = (response: Response, code: (typeof HttpStatusCode)[keyof typeof HttpStatusCode]) => {
+  const actual = response.status;
+
+  return {
+    pass: actual === code,
+    message: () => "Response has wrong status code",
+    actual,
+    expected: code,
+  };
+};
+
+export const status = {
+  isOk: (received: Response) => expectCode(received, HttpStatusCode.Ok),
+};
