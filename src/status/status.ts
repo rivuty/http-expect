ce6a25fda465799ed846a2752cdc3a764b120eef
Oleg Michailov <source.code@michailov.lt>
2026-03-21 11:37:15 +0200

    Add oxfmt formatter

--- src/status/status.ts
+++ src/status/status.ts
@@ -1,11 +1,11 @@
-import { HttpStatusCode } from "@rivuty/http-status";
+import { HttpStatusCode } from '@rivuty/http-status';
 
 const expectCode = (response: Response, code: (typeof HttpStatusCode)[keyof typeof HttpStatusCode]) => {
   const actual = response.status;
 
   return {
     pass: actual === code,
-    message: () => "Response has wrong status code",
+    message: () => 'Response has wrong status code',
     actual,
     expected: code,
   };
