36e477569b477ad86f11ae4e060870c9405553eb
Oleg Michailov <source.code@michailov.lt>
2026-03-24 18:04:52 +0200

    Add more http status matchers

--- src/status/StatusMatchers.test.ts
+++ src/status/StatusMatchers.test.ts
@@ -1,17 +1,80 @@
-// oxlint-disable typescript/no-confusing-void-expression
-
 import { describe, expect, it } from 'vitest';
 
 describe('StatusMatchers', () => {
-  describe('toHaveOkStatus', () => {
-    it('should pass when status is 200', () => {
-      const response = new Response('', { status: 200 });
-      expect(response).toHaveOkStatus();
-    });
+  it.each([
+    // 1xx Informational
+    ['toHaveContinueStatus', 100],
+    ['toHaveSwitchingProtocolsStatus', 101],
+    ['toHaveProcessingStatus', 102],
+    ['toHaveEarlyHintsStatus', 103],
+    // 2xx Success
+    ['toHaveOkStatus', 200],
+    ['toHaveCreatedStatus', 201],
+    ['toHaveAcceptedStatus', 202],
+    ['toHaveNonAuthoritativeInformationStatus', 203],
+    ['toHaveNoContentStatus', 204],
+    ['toHaveResetContentStatus', 205],
+    ['toHavePartialContentStatus', 206],
+    ['toHaveMultiStatusStatus', 207],
+    ['toHaveAlreadyReportedStatus', 208],
+    ['toHaveInstanceManipulationUsedStatus', 226],
+    // 3xx Redirection
+    ['toHaveMultipleChoicesStatus', 300],
+    ['toHaveMovedPermanentlyStatus', 301],
+    ['toHaveFoundStatus', 302],
+    ['toHaveSeeOtherStatus', 303],
+    ['toHaveNotModifiedStatus', 304],
+    ['toHaveUseProxyStatus', 305],
+    ['toHaveTemporaryRedirectStatus', 307],
+    ['toHavePermanentRedirectStatus', 308],
+    // 4xx Client Errors
+    ['toHaveBadRequestStatus', 400],
+    ['toHaveUnauthorizedStatus', 401],
+    ['toHavePaymentRequiredStatus', 402],
+    ['toHaveForbiddenStatus', 403],
+    ['toHaveNotFoundStatus', 404],
+    ['toHaveMethodNotAllowedStatus', 405],
+    ['toHaveNotAcceptableStatus', 406],
+    ['toHaveProxyAuthenticationRequiredStatus', 407],
+    ['toHaveRequestTimeoutStatus', 408],
+    ['toHaveConflictStatus', 409],
+    ['toHaveGoneStatus', 410],
+    ['toHaveLengthRequiredStatus', 411],
+    ['toHavePreconditionFailedStatus', 412],
+    ['toHavePayloadTooLargeStatus', 413],
+    ['toHaveUriTooLongStatus', 414],
+    ['toHaveUnsupportedMediaTypeStatus', 415],
+    ['toHaveRangeNotSatisfiableStatus', 416],
+    ['toHaveExpectationFailedStatus', 417],
+    ['toHaveMisdirectedRequestStatus', 421],
+    ['toHaveUnprocessableContentStatus', 422],
+    ['toHaveLockedStatus', 423],
+    ['toHaveFailedDependencyStatus', 424],
+    ['toHaveTooEarlyStatus', 425],
+    ['toHaveUpgradeRequiredStatus', 426],
+    ['toHavePreconditionRequiredStatus', 428],
+    ['toHaveTooManyRequestsStatus', 429],
+    ['toHaveRequestHeaderFieldsTooLargeStatus', 431],
+    ['toHaveUnavailableForLegalReasonsStatus', 451],
+    // 5xx Server Errors
+    ['toHaveInternalServerErrorStatus', 500],
+    ['toHaveNotImplementedStatus', 501],
+    ['toHaveBadGatewayStatus', 502],
+    ['toHaveServiceUnavailableStatus', 503],
+    ['toHaveGatewayTimeoutStatus', 504],
+    ['toHaveHttpVersionNotSupportedStatus', 505],
+    ['toHaveVariantAlsoNegotiatesStatus', 506],
+    ['toHaveInsufficientStorageStatus', 507],
+    ['toHaveLoopDetectedStatus', 508],
+    ['toHaveNotExtendedStatus', 510],
+    ['toHaveNetworkAuthenticationRequiredStatus', 511],
+  ] as const)('%s', (matcher, status) => {
+    expect({ status })[matcher]();
 
-    it('should fail when status is not 200', () => {
-      const response = new Response('', { status: 404 });
-      expect(() => expect(response).toHaveOkStatus()).toThrow('Response has 404 instead of 200 status code');
-    });
+    const other = status === 200 ? 404 : 200;
+    // oxlint-disable-next-line typescript/no-confusing-void-expression
+    expect(() => expect({ status: other })[matcher]()).toThrow(
+      `Response has ${other} instead of ${status} status code`,
+    );
   });
 });
--- src/status/StatusMatchers.ts
+++ src/status/StatusMatchers.ts
@@ -3,9 +3,153 @@ import { type ResponseLike } from './ResponseLike';
 import { expectStatus } from './expectStatus';
 
 export interface StatusMatchers<_> {
+  // 1xx Informational
+  toHaveContinueStatus: () => void;
+  toHaveSwitchingProtocolsStatus: () => void;
+  toHaveProcessingStatus: () => void;
+  toHaveEarlyHintsStatus: () => void;
+  // 2xx Success
   toHaveOkStatus: () => void;
+  toHaveCreatedStatus: () => void;
+  toHaveAcceptedStatus: () => void;
+  toHaveNonAuthoritativeInformationStatus: () => void;
+  toHaveNoContentStatus: () => void;
+  toHaveResetContentStatus: () => void;
+  toHavePartialContentStatus: () => void;
+  toHaveMultiStatusStatus: () => void;
+  toHaveAlreadyReportedStatus: () => void;
+  toHaveInstanceManipulationUsedStatus: () => void;
+  // 3xx Redirection
+  toHaveMultipleChoicesStatus: () => void;
+  toHaveMovedPermanentlyStatus: () => void;
+  toHaveFoundStatus: () => void;
+  toHaveSeeOtherStatus: () => void;
+  toHaveNotModifiedStatus: () => void;
+  toHaveUseProxyStatus: () => void;
+  toHaveTemporaryRedirectStatus: () => void;
+  toHavePermanentRedirectStatus: () => void;
+  // 4xx Client Errors
+  toHaveBadRequestStatus: () => void;
+  toHaveUnauthorizedStatus: () => void;
+  toHavePaymentRequiredStatus: () => void;
+  toHaveForbiddenStatus: () => void;
+  toHaveNotFoundStatus: () => void;
+  toHaveMethodNotAllowedStatus: () => void;
+  toHaveNotAcceptableStatus: () => void;
+  toHaveProxyAuthenticationRequiredStatus: () => void;
+  toHaveRequestTimeoutStatus: () => void;
+  toHaveConflictStatus: () => void;
+  toHaveGoneStatus: () => void;
+  toHaveLengthRequiredStatus: () => void;
+  toHavePreconditionFailedStatus: () => void;
+  toHavePayloadTooLargeStatus: () => void;
+  toHaveUriTooLongStatus: () => void;
+  toHaveUnsupportedMediaTypeStatus: () => void;
+  toHaveRangeNotSatisfiableStatus: () => void;
+  toHaveExpectationFailedStatus: () => void;
+  toHaveMisdirectedRequestStatus: () => void;
+  toHaveUnprocessableContentStatus: () => void;
+  toHaveLockedStatus: () => void;
+  toHaveFailedDependencyStatus: () => void;
+  toHaveTooEarlyStatus: () => void;
+  toHaveUpgradeRequiredStatus: () => void;
+  toHavePreconditionRequiredStatus: () => void;
+  toHaveTooManyRequestsStatus: () => void;
+  toHaveRequestHeaderFieldsTooLargeStatus: () => void;
+  toHaveUnavailableForLegalReasonsStatus: () => void;
+  // 5xx Server Errors
+  toHaveInternalServerErrorStatus: () => void;
+  toHaveNotImplementedStatus: () => void;
+  toHaveBadGatewayStatus: () => void;
+  toHaveServiceUnavailableStatus: () => void;
+  toHaveGatewayTimeoutStatus: () => void;
+  toHaveHttpVersionNotSupportedStatus: () => void;
+  toHaveVariantAlsoNegotiatesStatus: () => void;
+  toHaveInsufficientStorageStatus: () => void;
+  toHaveLoopDetectedStatus: () => void;
+  toHaveNotExtendedStatus: () => void;
+  toHaveNetworkAuthenticationRequiredStatus: () => void;
 }
 
 export const statusMatchers = {
+  // 1xx Informational
+  toHaveContinueStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Continue),
+  toHaveSwitchingProtocolsStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.SwitchingProtocols),
+  toHaveProcessingStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Processing),
+  toHaveEarlyHintsStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.EarlyHints),
+  // 2xx Success
   toHaveOkStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Ok),
+  toHaveCreatedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Created),
+  toHaveAcceptedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Accepted),
+  toHaveNonAuthoritativeInformationStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.NonAuthoritativeInformation),
+  toHaveNoContentStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.NoContent),
+  toHaveResetContentStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.ResetContent),
+  toHavePartialContentStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.PartialContent),
+  toHaveMultiStatusStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.MultiStatus),
+  toHaveAlreadyReportedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.AlreadyReported),
+  toHaveInstanceManipulationUsedStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.InstanceManipulationUsed),
+  // 3xx Redirection
+  toHaveMultipleChoicesStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.MultipleChoices),
+  toHaveMovedPermanentlyStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.MovedPermanently),
+  toHaveFoundStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Found),
+  toHaveSeeOtherStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.SeeOther),
+  toHaveNotModifiedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.NotModified),
+  toHaveUseProxyStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.UseProxy),
+  toHaveTemporaryRedirectStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.TemporaryRedirect),
+  toHavePermanentRedirectStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.PermanentRedirect),
+  // 4xx Client Errors
+  toHaveBadRequestStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.BadRequest),
+  toHaveUnauthorizedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Unauthorized),
+  toHavePaymentRequiredStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.PaymentRequired),
+  toHaveForbiddenStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Forbidden),
+  toHaveNotFoundStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.NotFound),
+  toHaveMethodNotAllowedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.MethodNotAllowed),
+  toHaveNotAcceptableStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.NotAcceptable),
+  toHaveProxyAuthenticationRequiredStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.ProxyAuthenticationRequired),
+  toHaveRequestTimeoutStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.RequestTimeout),
+  toHaveConflictStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Conflict),
+  toHaveGoneStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Gone),
+  toHaveLengthRequiredStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.LengthRequired),
+  toHavePreconditionFailedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.PreconditionFailed),
+  toHavePayloadTooLargeStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.PayloadTooLarge),
+  toHaveUriTooLongStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.UriTooLong),
+  toHaveUnsupportedMediaTypeStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.UnsupportedMediaType),
+  toHaveRangeNotSatisfiableStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.RangeNotSatisfiable),
+  toHaveExpectationFailedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.ExpectationFailed),
+  toHaveMisdirectedRequestStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.MisdirectedRequest),
+  toHaveUnprocessableContentStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.UnprocessableContent),
+  toHaveLockedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.Locked),
+  toHaveFailedDependencyStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.FailedDependency),
+  toHaveTooEarlyStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.TooEarly),
+  toHaveUpgradeRequiredStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.UpgradeRequired),
+  toHavePreconditionRequiredStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.PreconditionRequired),
+  toHaveTooManyRequestsStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.TooManyRequests),
+  toHaveRequestHeaderFieldsTooLargeStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.RequestHeaderFieldsTooLarge),
+  toHaveUnavailableForLegalReasonsStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.UnavailableForLegalReasons),
+  // 5xx Server Errors
+  toHaveInternalServerErrorStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.InternalServerError),
+  toHaveNotImplementedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.NotImplemented),
+  toHaveBadGatewayStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.BadGateway),
+  toHaveServiceUnavailableStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.ServiceUnavailable),
+  toHaveGatewayTimeoutStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.GatewayTimeout),
+  toHaveHttpVersionNotSupportedStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.HttpVersionNotSupported),
+  toHaveVariantAlsoNegotiatesStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.VariantAlsoNegotiates),
+  toHaveInsufficientStorageStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.InsufficientStorage),
+  toHaveLoopDetectedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.LoopDetected),
+  toHaveNotExtendedStatus: (received: ResponseLike) => expectStatus(received, HttpStatusCode.NotExtended),
+  toHaveNetworkAuthenticationRequiredStatus: (received: ResponseLike) =>
+    expectStatus(received, HttpStatusCode.NetworkAuthenticationRequired),
 };
